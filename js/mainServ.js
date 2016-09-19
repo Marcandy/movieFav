8bb65b02966cfc2e0f0ce19b70885c16fd558691
marcandy <marcandy.nj@gmail.com>
2016-09-19 14:16:40 -0500

    fixing https request

--- js/mainServ.js
+++ js/mainServ.js
@@ -1,10 +1,10 @@
 angular.module('movieFav').service('mainServ', function ($http, $q) {
 var page = 1;
-var baseUrl= 'http://api.themoviedb.org/3/movie/popular?api_key=992bee131be669ca2f75ef3b62f31264';
+var baseUrl= 'https://api.themoviedb.org/3/movie/popular?api_key=992bee131be669ca2f75ef3b62f31264';
 
-var searchUrl = 'http://api.themoviedb.org/3/search/movie?api_key=992bee131be669ca2f75ef3b62f31264&query=';
+var searchUrl = 'https://api.themoviedb.org/3/search/movie?api_key=992bee131be669ca2f75ef3b62f31264&query=';
 
-var infoUrl = 'http://api.themoviedb.org/3/movie/';
+var infoUrl = 'https://api.themoviedb.org/3/movie/';
 
 var topUrl = 'https://api.themoviedb.org/3/movie/top_rated';
 
