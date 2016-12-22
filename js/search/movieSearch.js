fe3d97070b803dd40519271c524722195a21e6cd
marcandy <marcandy.nj@gmail.com>
2016-12-21 17:07:22 -0700

    few improv

--- js/search/movieSearch.js
+++ js/search/movieSearch.js
@@ -1,13 +1,28 @@
 angular.module('movieFav').controller('movieSearch', function($scope, $state, mainServ) {
 
   $scope.viewData = function(movieId) {
+    $scope.getVideo(movieId)
     mainServ.getInfo(movieId)
       .then(function (info) {
-        console.log(info)
 
+        console.log(info)
         mainServ.savedInfo = info;
+
         $state.go('popup');
 
   })
 }
+
+$scope.getVideo = function(movieId) {
+    mainServ.getVideo(movieId)
+      .then(function (video) {
+
+        console.log(video)
+        mainServ.key = video;
+
+        // $state.go('popup');
+
+  })
+}
+
 });
